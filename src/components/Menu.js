5eafe683767318cb2ed7b6939483f5a3eec49892
mariuszo52 <mariuszo19@onet.pl>
2023-09-22 12:55:27 +0200

    update home page view

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping, faUser, faMagnifyingGlass, faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping, faUser, faHeart } from '@fortawesome/free-solid-svg-icons'
 import logo from "../images/logo.png"
 function Menu() {
     return (
         <div className={"menu-div"}>
             <div className={"menu-panel"}>
-                <a href={"#"}> <p>Menu item</p></a>
-                <a href={"#"}> <p>Menu item</p></a>
-                <a href={"#"}> <p>Menu item</p></a>
-                <a href={"#"}> <p>Menu item</p></a>
-                <a href={"#"}> <p>Menu item</p></a>
+                <a href={"#"}> <p>PC</p></a>
+                <a href={"#"}> <p>PSN</p></a>
+                <a href={"#"}> <p>XBOX</p></a>
+                <a href={"#"}> <p>NINTENDO</p></a>
+                <a href={"#"}> <p>TOP-UPS</p></a>
                 </div>
             <div className={"logo-div"}>
                 <img className={"logo"} src={logo}/>
             </div>
             <div className={"user-panel"}>
                 <a href={"#"}><FontAwesomeIcon icon={faUser}/></a>
-                <a href={"#"}><FontAwesomeIcon icon={faMagnifyingGlass} /></a>
                 <a href={"#"}><FontAwesomeIcon icon={faHeart}/></a>
                 <a href={"#"}><FontAwesomeIcon icon={faCartShopping} /></a>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,24 +4,15 @@ import games1 from "../images/games-photos/1.jpg"
 import games2 from "../images/games-photos/2.jpg"
 import games3 from "../images/games-photos/3.jpg"
 import cover from "../images/game-covers/wiedzmin.jpg"
-import axios from "axios";
-
+import tiktokLogo from "../images/social-media/tiktok.png";
+import instagramLogo from "../images/social-media/instagram.png";
+import facebookLogo from "../images/social-media/facebook.png";
+import cardLogo from "../images/social-media/card.png";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 function Home() {
     const [slideIndex, setSlideIndex] = useState(1);
 
-    useEffect(() => {
-        const config = {
-            headers: {
-                'X-Api-Key': '5cbbfc909dc69600f9406469c0775254'
-            },
-        };
-        function getProducts(){
-            axios.get("https://gateway.kinguin.net/esa/api/v1/products?name=fifa",config)
-                .then(res => console.log(res.data))
-                .catch(err => console.log(err))
-        }
-        getProducts()
-    }, []);
     function handleMouseOut(){
         let span = document.getElementsByClassName("add-to-cart");
         for (let spanElement of span) {
@@ -62,9 +53,9 @@ function Home() {
         <div className={"main-div"}>
             <Menu/>
             <div className="slideshow-container">
-                <img id={"first-slide"} className={"slide"} src={games1}/>
-                <img className={"slide"} src={games2}/>
-                <img className={"slide"} src={games3}/>
+                <img alt={"slide1"} id={"first-slide"} className={"slide"} src={games1}/>
+                <img alt={"slide2"} className={"slide"} src={games2}/>
+                <img alt={"slide3"} className={"slide"} src={games3}/>
             </div>
                 <ul className={"section-list"}>
                     <li className={"section-list-el"}>News</li>
@@ -72,6 +63,12 @@ function Home() {
                     <li className={"section-list-el"}>Secondhand</li>
                     <li className={"section-list-el"}>Preorder</li>
                 </ul>
+            <br/>
+            <form className={"search-form"}>
+                <input type={"text"}/>
+                <FontAwesomeIcon id={"submit-search-icon"} icon={faMagnifyingGlass} />
+            </form>
+
             <div className={"products-div"}>
                 <ul className={"products-list"}>
                     <li onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className={"products-list-el"}>
@@ -116,7 +113,33 @@ function Home() {
                         <p>GAME Price</p>
                     </li>
                 </ul>
+                <div className={"pagination-bar"}>
+                    <p>20 of 50 games loaded</p>
+                    <div className={"pages-numbers"}>
+                        <p>prev</p>
+                        <p>1</p>
+                        <p>2</p>
+                        <p>3</p>
+                        <p>next</p>
+                    </div>
+                </div>
             </div>
+            <div className={"info-div"}>
+                <div className={"social-media-div"}>
+                    <p>SOCIAL MEDIA</p>
+                    <img alt={"facebook"} src={facebookLogo} />
+                    <img alt={"instagram"} src={instagramLogo} />
+                    <img alt={"tiktok"} src={tiktokLogo} />
+                </div>
+                <div className={"payment-methods-div"}>
+                    <p>PAYMENT METHODS</p>
+                    <img alt={"card"} src={cardLogo} />
+
+                </div>
+            </div>
+            <footer className={"footer"}>
+                <p>Copyright cd-keys.com.pl 2023, all rights reserved</p>
+            </footer>
         </div>
     );
 }
