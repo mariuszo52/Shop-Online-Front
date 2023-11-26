5072bf0dea14587b4f8115fc54cb016366fb8a08
mariuszo52 <the506miz@gmail.com>
2023-11-26 16:03:16 +0100

    update UserPanelPage.js

--- src/pages/UserPanelPage.js
+++ src/pages/UserPanelPage.js
@@ -1,6 +1,16 @@
-function UserPanelPage(){
-    return(
-        <div className={"main-div"}></div>
+import Menu from "../components/Menu";
+
+function UserPanelPage() {
+    return (
+        <div className={"main-div"}>
+            <Menu/>
+            <div className={"account-panel-container"}>
+            <div className={"account-menu"}>
+
+            </div>
+            </div>
+        </div>
     )
 }
+
 export default UserPanelPage;
