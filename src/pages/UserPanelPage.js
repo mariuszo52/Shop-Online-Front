58cfab2f0865d9d1833979215bbf6b1f54384076
mariuszo52 <the506miz@gmail.com>
2023-11-26 20:17:50 +0100

    update UserPanelPage.js

--- src/pages/UserPanelPage.js
+++ src/pages/UserPanelPage.js
@@ -6,8 +6,21 @@ function UserPanelPage() {
             <Menu/>
             <div className={"account-panel-container"}>
             <div className={"account-menu"}>
-
+            <p className={"account-menu-el"}>MY ACCOUNT</p>
+                <p className={"account-menu-el"}>MY ORDERS</p>
+                <p className={"account-menu-el"}>WISHLIST</p>
+                <p className={"account-menu-el"}>MESSAGES</p>
+                <p className={"account-menu-el"}>LOGOUT</p>
             </div>
+                <div className={"menu-my-account"}>
+                    <h1>MY ACCOUNT</h1>
+                    <div className={"my-account-header"}>
+                        <h3>ACCOUNT INFORMATION</h3>
+                    </div>
+                    <div className={"account-information-div"}>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
