01fe176f5b2783396b9c3c502048553fdfe88143
mariuszo52 <the506miz@gmail.com>
2024-01-06 18:13:46 +0100

    update users management component

--- src/components/adminPanelPage/MenageUsers.js
+++ src/components/adminPanelPage/MenageUsers.js
@@ -1,12 +1,16 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import EditPanel from "./EditPanel";
+import {useNotification} from "../../context/NotificationContext";
 
 function MenageUsers() {
     const [users, setUsers] = useState([])
     const [isElementClicked, setIsElementClicked] = useState(false)
-    const [editInputValue, setEditInputValue] = useState("")
-
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [isEnabled, setIsEnabled] = useState(false)
+    const [role, setRole] = useState("")
+    const {setNotificationVisible, setNotificationText} = useNotification();
     useEffect(() => {
         function fetchAllUsers() {
             axios.get("http://localhost:8080/user-management/all-users")
@@ -20,19 +24,25 @@ function MenageUsers() {
         fetchAllUsers()
     }, []);
 
-    function onListElementClick(index, name) {
+    function showElementEditor(index, name, focusElementName) {
         if (!isElementClicked) {
             let span = document.getElementById("edit-span-" + name + index)
             let form = document.getElementById("edit-form-" + name + index);
-            let input = form.getElementsByTagName("input").item(0);
+            let focusElement
+            if (focusElementName === "form") {
+                focusElement = form
+            } else {
+                focusElement = form.getElementsByTagName(focusElementName).item(0);
+            }
             span.style.display = "none"
             form.style.display = "flex"
             setIsElementClicked(true)
-            input.focus()
-            input.addEventListener("blur", ev => closeForm(form, span) )
+            focusElement.focus()
+            focusElement.addEventListener("blur", ev => closeForm(form, span))
         }
     }
-    function closeForm(form, span){
+
+    function closeForm(form, span) {
         span.style.display = "flex"
         form.style.display = "none"
         setIsElementClicked(false)
@@ -42,27 +52,44 @@ function MenageUsers() {
 
     function updateUserField(fieldName, userId, index) {
         let url;
-        switch (fieldName){
-            case "username": url = "http://localhost:8080/user-management/username"
+        let value;
+        switch (fieldName) {
+            case "username":
+                url = "http://localhost:8080/user-management/username"
+                value = username
                 break
-            case "email": url = "http://localhost:8080/user-management/email"
+            case "email":
+                url = "http://localhost:8080/user-management/email"
+                value = email
+                break
+            case "isEnabled":
+                url = "http://localhost:8080/user-management/is-enabled"
+                value = isEnabled
+                break
+            case "role":
+                url = "http://localhost:8080/user-management/role"
+                value = role
         }
         let data = {
             userId: userId,
-            [fieldName]: editInputValue
+            [fieldName]: value
         }
         axios.put(url, data)
             .then(response => {
                 let span = document.getElementById("edit-span-" + fieldName + index)
                 let form = document.getElementById("edit-form-" + fieldName + index);
+                span.innerText = value;
                 span.style.display = "flex"
                 form.style.display = "none"
             })
-            .catch(reason => console.log(reason))
+            .catch(reason => {
+                setNotificationText(reason.response.data)
+                setNotificationVisible(true)
+                console.log(reason)
+            })
     }
 
-
-    function onUpdateSubmit (event, fieldName, userId, index) {
+    function onUpdateSubmit(event, fieldName, userId, index) {
         event.preventDefault()
         updateUserField(fieldName, userId, index)
         setIsElementClicked(false)
@@ -89,35 +116,75 @@ function MenageUsers() {
                         <td
                             className={"username"}
                             onClick={event =>
-                                onListElementClick(index, "username")}
-                        ><form
-                            onSubmit={event =>
-                                onUpdateSubmit(event, "username", user?.id, index)}
-                            id={"edit-form-username" + index}
-                            className={"edit-users-form"}>
-                            <input
-                                required={true}
-                                value={editInputValue}
-                                onChange={event => setEditInputValue(event.target.value)}
-                            />
-                        </form>
+                                showElementEditor(index, "username", "input")}
+                        >
+                            <form
+                                onSubmit={event =>
+                                    onUpdateSubmit(event, "username", user?.id, index)}
+                                id={"edit-form-username" + index}
+                                className={"edit-users-form"}>
+                                <input
+                                    required={true}
+                                    value={username}
+                                    onChange={event => setUsername(event.target.value)}
+                                />
+                            </form>
                             <span id={"edit-span-username" + index}>{user?.username}</span></td>
                         <td
                             className={"email"}
                             onClick={(event) =>
-                                onListElementClick(index, "email")}>
+                                showElementEditor(index, "email", "input")}>
                             <form
+                                onSubmit={event =>
+                                    onUpdateSubmit(event, "email", user?.id, index)}
                                 id={"edit-form-email" + index}
                                 className={"edit-users-form"}>
                                 <input
+                                    type={"email"}
                                     required={true}
-                                    value={editInputValue}
-                                    onChange={event => setEditInputValue(event.target.value)}
+                                    value={email}
+                                    onChange={event => setEmail(event.target.value)}
                                 />
                             </form>
                             <span id={"edit-span-email" + index}>{user?.email}</span></td>
-                        <td>{user?.isEnabled.toString()}</td>
-                        <td>{user?.userRole}</td>
+                        <td
+                            className={"isEnabled"}
+                            onClick={(event) =>
+                                showElementEditor(index, "isEnabled", "form")}>
+                            <form
+                                onSubmit={event =>
+                                    onUpdateSubmit(event, "isEnabled", user?.id, index)}
+                                id={"edit-form-isEnabled" + index}
+                                className={"edit-users-form"}>
+                                <select onChange={(event) => {
+                                    setIsEnabled(event.target.value);
+                                }}
+                                >
+                                    <option value={"false"}>FALSE</option>
+                                    <option value={"true"}>TRUE</option>
+                                </select>
+                                <button type={"submit"}>OK</button>
+                            </form>
+                            <span id={"edit-span-isEnabled" + index}>{user?.isEnabled.toString()}</span></td>
+                        <td
+                            className={"role"}
+                            onClick={(event) =>
+                                showElementEditor(index, "role", "form")}>
+                            <form
+                                onSubmit={event =>
+                                    onUpdateSubmit(event, "role", user?.id, index)}
+                                id={"edit-form-role" + index}
+                                className={"edit-users-form"}>
+                                <select onChange={(event) => {
+                                    setRole(event.target.value);
+                                }}
+                                >
+                                    <option value={"ADMIN"}>ADMIN</option>
+                                    <option value={"USER"}>USER</option>
+                                </select>
+                                <button type={"submit"}>OK</button>
+                            </form>
+                            <span id={"edit-span-role" + index}>{user?.userRole}</span></td>
                         <td>{user?.userInfo.id}</td>
                     </tr>
                     </tbody>
