216cdeaaa820408332a452ab8533174ecd574bd7
mariuszo52 <the506miz@gmail.com>
2024-02-13 15:55:30 +0100

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from "./App";
 import AxiosInterceptor from "./interceptors/AxiosInterceptor";
 import "./components/i18n"
 const root = ReactDOM.createRoot(document.getElementById('root'));
+import HttpsRedirect from 'react-https-redirect';
 root.render(
-    <>
+    <HttpsRedirect>
         <AxiosInterceptor />
         <App />
-    </>
+    </HttpsRedirect>
 );
